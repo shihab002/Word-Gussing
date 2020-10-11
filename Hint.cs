62483490a003eb20708a76649c6e04325b126354
shihab002 <72697139+shihab002@users.noreply.github.com>
2020-10-11 19:28:33 +0530

    Add files via upload

---
+++ Hint.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hint : MonoBehaviour
+{
+    public Text HintShow = null;
+    public void ChangeHint(string word)
+    {
+        HintShow.text = word;
+    }
+}
